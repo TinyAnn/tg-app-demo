41ef454dd9a2b520a34c5fe379a1824c6d076067
TinyAnn <522988582@qq.com>
2016-05-03 11:23:58 +0800

    hot-module

--- src/js/test.jsx
+++ src/js/test.jsx
@@ -3,6 +3,10 @@ import { render } from 'react-dom';
 
 import Header from '../components/header/Header';
 
+//启动热替换
+if(module.hot)
+    module.hot.accept();
+
 render(
 	<div>
 		<Header title='实名认证' />
